7ac630d585ee901827e87b15f2932187c3345f92
Ye Joo Park <subwaymatch@gmail.com>
2020-07-08 06:10:53 -0500

    Implement addTodo ID generation

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,12 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+let nextTodoId = 0;
+
 const todoSlice = createSlice({
     name: "todos",
     initialState: [],
     reducers: {
-        addTodo(state, action) {
-            const { id, text } = action.payload;
-            state.push({ id, text, completed: false });
+        addTodo: {
+            reducer(state, action) {
+                const { id, text } = action.payload;
+                state.push({ id, text, completed: false });
+            },
+            prepare(text) {
+                return {
+                    payload: { text, id: nextTodoId++ },
+                };
+            },
         },
         toogleTodo(state, action) {
             const todo = state.find((todo) => todo.id === action.payload);
