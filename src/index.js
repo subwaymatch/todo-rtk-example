c4ddac23096307e65933e36c34fd479dea319aa3
Ye Joo Park <subwaymatch@gmail.com>
2020-07-08 01:08:12 -0500

    Working version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import App from "./components/App";
 
-ReactDOM.render(<div>Test</div>, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
