fb26c7b040ded1c4c18b5d6d86bbad2dbd8f936b
Ye Joo Park <subwaymatch@gmail.com>
2020-07-08 07:04:04 -0500

    Use the filters slice reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 import todosReducer from "features/todos/todosSlice";
-import visibilityFilter from "./visibilityFilter";
+import visibilityFilterReducer from "features/filters/filterSlice";
 
 export default combineReducers({
     todos: todosReducer,
-    visibilityFilter,
+    visibilityFilter: visibilityFilterReducer,
 });
